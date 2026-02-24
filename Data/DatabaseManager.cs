780f5ae3ead467bcc1fca9210a655e83f40e5dca
Raygunpewpew1 <raygunpewpew@gmail.com>
2026-02-24 01:52:02 -0600

    db cache rem

--- Data/DatabaseManager.cs
+++ Data/DatabaseManager.cs
@@ -61,8 +61,8 @@ public sealed class DatabaseManager : IDisposable
 
                     // Create collection tables
                     await ExecuteNonQueryAsync(_collectionConnection, SQLQueries.CreateCollectionTable);
-                    await ExecuteNonQueryAsync(_collectionConnection, SQLQueries.CreateThumbnailCacheTable);
-                    await ExecuteNonQueryAsync(_collectionConnection, SQLQueries.CreateThumbnailIndexAccessed);
+                //    await ExecuteNonQueryAsync(_collectionConnection, SQLQueries.CreateThumbnailCacheTable);
+               //     await ExecuteNonQueryAsync(_collectionConnection, SQLQueries.CreateThumbnailIndexAccessed);
                     await MigrateCollectionSchemaAsync(_collectionConnection);
 
                     _isConnected = true;
@@ -176,8 +176,8 @@ public sealed class DatabaseManager : IDisposable
         await _collectionConnection.OpenAsync();
         await ConfigureConnectionAsync(_collectionConnection);
         await ExecuteNonQueryAsync(_collectionConnection, SQLQueries.CreateCollectionTable);
-        await ExecuteNonQueryAsync(_collectionConnection, SQLQueries.CreateThumbnailCacheTable);
-        await ExecuteNonQueryAsync(_collectionConnection, SQLQueries.CreateThumbnailIndexAccessed);
+    //    await ExecuteNonQueryAsync(_collectionConnection, SQLQueries.CreateThumbnailCacheTable);
+   //     await ExecuteNonQueryAsync(_collectionConnection, SQLQueries.CreateThumbnailIndexAccessed);
         await MigrateCollectionSchemaAsync(_collectionConnection);
     }
 
