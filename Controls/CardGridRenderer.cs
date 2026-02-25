45a336728f2d4a97c50799d619b0860f84ad2e88
Raygunpewpew1 <raygunpewpew@gmail.com>
2026-02-25 05:30:13 -0600

    Update CardGridRenderer.cs

--- Controls/CardGridRenderer.cs
+++ Controls/CardGridRenderer.cs
@@ -53,16 +53,16 @@ internal sealed class CardGridRenderer : IDisposable
     public void EnsureResources()
     {
         _bgPaint ??= new SKPaint { Color = new SKColor(30, 30, 30), IsAntialias = true };
-        _textPaint ??= new SKPaint { Color = SKColors.White, IsAntialias = true, SubpixelText = true };
-        _textFont ??= new SKFont(SKTypeface.FromFamilyName("sans-serif", SKFontStyle.Normal), 12f);
-        _pricePaint ??= new SKPaint { Color = SKColors.LightGreen, IsAntialias = true, SubpixelText = true };
-        _priceFont ??= new SKFont(SKTypeface.FromFamilyName("sans-serif", SKFontStyle.Normal), 8.5f);
+        _textPaint ??= new SKPaint { Color = SKColors.White, IsAntialias = true };
+        _textFont ??= new SKFont(SKTypeface.FromFamilyName("CrimsonText-Regular", SKFontStyle.Normal), 12f) { Subpixel = true };
+        _pricePaint ??= new SKPaint { Color = SKColors.LightGreen, IsAntialias = true };
+        _priceFont ??= new SKFont(SKTypeface.FromFamilyName("CrimsonText-Regular", SKFontStyle.Normal), 8.5f) { Subpixel = true };
         _badgeBgPaint ??= new SKPaint { IsAntialias = true, Color = new SKColor(220, 50, 50) };
-        _badgeTextPaint ??= new SKPaint { IsAntialias = true, Color = SKColors.White, SubpixelText = true };
-        _badgeFont ??= new SKFont(SKTypeface.FromFamilyName("sans-serif", SKFontStyle.Bold), 11f);
+        _badgeTextPaint ??= new SKPaint { IsAntialias = true, Color = SKColors.White };
+        _badgeFont ??= new SKFont(SKTypeface.FromFamilyName("CrimsonText-Bold", SKFontStyle.Bold), 11f) { Subpixel = true };
         _shimmerBasePaint ??= new SKPaint { Color = new SKColor(40, 40, 40) };
-        _secondaryTextPaint ??= new SKPaint { Color = new SKColor(160, 160, 160), IsAntialias = true, SubpixelText = true };
-        _secondaryTextFont ??= new SKFont(SKTypeface.FromFamilyName("sans-serif", SKFontStyle.Normal), 11f);
+        _secondaryTextPaint ??= new SKPaint { Color = new SKColor(160, 160, 160), IsAntialias = true };
+        _secondaryTextFont ??= new SKFont(SKTypeface.FromFamilyName("CrimsonText-Regular", SKFontStyle.Normal), 11f) { Subpixel = true };
         _separatorPaint ??= new SKPaint { Color = new SKColor(50, 50, 50) };
         _cardRoundRect ??= new SKRoundRect();
         _imageRoundRect ??= new SKRoundRect();
