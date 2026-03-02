4c88dfcb015000723eec9e0e785d64b3ab829fcb
Raygunpewpew1 <raygunpewpew@gmail.com>
2026-03-02 04:13:04 -0600

    muffin

--- Data/DeckRepository.cs
+++ Data/DeckRepository.cs
@@ -1,5 +1,5 @@
-using Microsoft.Data.Sqlite;
 using AetherVault.Models;
+using Microsoft.Data.Sqlite;
 
 namespace AetherVault.Data;
 
--- ViewModels/CollectionViewModel.cs
+++ ViewModels/CollectionViewModel.cs
@@ -1,10 +1,10 @@
-using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
 using AetherVault.Controls;
 using AetherVault.Core;
 using AetherVault.Core.Layout;
 using AetherVault.Models;
 using AetherVault.Services;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace AetherVault.ViewModels;
 
